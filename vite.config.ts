c2aaeb2db697b8acfcc5a9094be065b36befcb49
JosemaRM46 <romeromanuel@unah.hn>
2024-10-31 19:58:05 -0600

    changes in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //borrar
-  server: {
-    host: '0.0.0.0',
-    port: 5173
+  build: {
+    outDir: 'dist'
   }
 })
